56a60bdc7bca9e14ab44ef7b497f70fef4e25199
Alexandra Krikunova <akrikuno@c4c2c5.42berlin.de>
2023-01-18 19:40:19 +0100

    get_next_line

---
+++ get_next_line_to_submit/get_next_line.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrikuno <akrikuno@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/06 19:00:49 by akrikuno          #+#    #+#             */
+/*   Updated: 2023/01/11 17:13:20 by akrikuno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_read(int fd, char *str)
+{
+	char	*buff;
+	int		bytes;
+
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(str, '\n') && bytes != 0)
+	{
+		bytes = read(fd, buff, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(buff);
+			free(str);
+			return (NULL);
+		}
+		buff[bytes] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	free(buff);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	str = ft_read(fd, str);
+	if (!str)
+		return (NULL);
+	line = ft_line(str);
+	str = ft_next_str(str);
+	return (line);
+}
+
+/*int	main(void)
+{
+	int fd;
+	char *tab;
+	fd = open("test.txt", O_RDONLY);
+	tab = get_next_line(fd);
+	printf("%s", tab);
+	free(tab);
+	tab = get_next_line(fd);
+	printf("%s", tab);
+	tab = get_next_line(fd);
+	printf("%s", tab);
+	tab = get_next_line(fd);
+	printf("%s", tab);
+	close(fd);
+	return (0);
+}*/
---
+++ get_next_line_to_submit/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akrikuno <akrikuno@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 15:30:02 by akrikuno          #+#    #+#             */
+/*   Updated: 2023/01/11 17:53:16 by akrikuno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+char	*ft_line(char *str);
+char	*ft_next_str(char *str);
+char	*ft_read(int fd, char *str);
+char	*get_next_line(int fd);
+
+#endif
